c2928aaa9c71d7aabda90d69022e60353c2d256d
TTong74 <ttong@seattleu.edu>
2020-01-22 19:13:32 -0800

    finished 2-fizzbuzz-fun

--- 2-fizzbuzz-fun/index.js
+++ 2-fizzbuzz-fun/index.js
@@ -1,8 +1,11 @@
 /*
-Description:
-Input:
-Output:
-Usage: 
+Description: Program that looks for an int argument using a function, else display a message showing the format of the command
+Input: Integer
+Output: List of values from 1 to the value of the integer
+Value / 3 = "fizz"
+Value / 5 = "buzz"
+Value / 15 = "fizzbuzz" 
+Usage: node 2-fizzbuzz-fun <integer> 
 */
 
 // get input using getargs
@@ -10,17 +13,23 @@ const getargs = require("../modules/getargs")
 let input = getargs.getIntegerArg()
 
 
-function fizzbuzz(n){
-// TODO provide for tests for divisible for 3,5, and 15 to return desired output
+function fizzbuzz(n) {
+  if (n % 15 == 0) {
+    return 'fizzbuzz';
+  } else if (n % 3 == 0) {
+    return 'fizz';
+  } else if (n % 5 == 0) {
+    return 'buzz';
+  } else {
+    return '';
+  }
 }
 
-// TODO check if input is not an integer
-if ( ) {
-  // TODO provide usage statement
+if (isNaN(input) || !Number.isInteger(input)) {
+  console.log("usage: node 2-fizzbuzz-fun <integer>");
 }
 else {
-  // TODO provide info to for loop to iterate from 1 to value of input
-  for ( ) {
+  for (let i = 1; i <= input; i++) {
     console.log(`${i} ${fizzbuzz(i)}`);
   }
 }
